b8c5abc6ba275ec5feafc984db89f6a71ecbea99
Orietta Roperto <39174444+oriettaroperto@users.noreply.github.com>
2018-09-30 10:23:37 -0400

    remove dot, sw and swregister from cached files

--- sw.js
+++ sw.js
@@ -10,26 +10,24 @@ let cachedVersion = 'restaurant-static-v1';
 * Contents to cache
 **/
 let cacheFiles = [
-  './',
-  './index.html',
-  './restaurant.html',
-  './sw.js',
-  './css/styles.css',
-  './js/dbhelper.js',
-  './js/main.js',
-  './js/restaurant_info.js',
-  './js/swregister.js',
-  './data/restaurants.json',
-  './img/1.jpg',
-  './img/2.jpg',
-  './img/3.jpg',
-  './img/4.jpg',
-  './img/5.jpg',
-  './img/6.jpg',
-  './img/7.jpg',
-  './img/8.jpg',
-  './img/9.jpg',
-  './img/10.jpg'
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg'
 ];
 
 self.addEventListener('install', function(event) {
